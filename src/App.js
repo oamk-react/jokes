969346a5a98cefce25576af390fe14580cf843f2
Jouni Juntunen <jouni.juntunen@oamk.fi>
2021-08-12 14:14:44 +0300

    Finished example.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+ 
+const URL = 'https://official-joke-api.appspot.com/random_joke';
 
 function App() {
+  const [setup,setSetup] = useState('Get random (and bad) jokes!');
+  const [punchline,setPunchline] = useState('Start by pressing that button');
+
+  async function getJoke(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch(URL);
+
+      if (response.ok) {
+        const json = await response.json();
+        setSetup(json.setup);
+        setPunchline(json.punchline);
+      } else {
+        alert('Error retrieving joke!');
+      }
+    } catch (err) {
+      alert(err);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin:50}}>
+      <h3>Random jokes</h3>
+      <form onSubmit={getJoke}>
+        <p>{setup}</p>
+        <p>{punchline}</p>
+        <button>Get another one</button>
+      </form>
     </div>
   );
 }
